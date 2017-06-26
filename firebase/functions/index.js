466bb1a08bbba78fde2add688534b6215f341d26
Larry Tin <dev@goodow.com>
2017-06-26 10:43:03 +0800

    Add firebase

---
+++ firebase/functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+
+// Max number of lines of the chat history.
+const MAX_LOG_COUNT = 10;
+
+// Removes siblings of the node that element that triggered the function if there are more than MAX_LOG_COUNT.
+// In this example we'll keep the max number of chat message history to MAX_LOG_COUNT.
+exports.truncateBusHistory = functions.database.ref('/bus/history/{topicId}/messages/{messageId}').onWrite(event => {
+      const parentRef = event.data.ref.parent;
+return parentRef.once('value').then(snapshot => {
+      if (snapshot.numChildren() >= MAX_LOG_COUNT) {
+  let childCount = 0;
+  const updates = {};
+  snapshot.forEach(function(child) {
+    if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
+      updates[child.key] = null;
+    }
+  });
+  // Update the parent. This effectively removes the extra children.
+  return parentRef.update(updates);
+}
+});
+});
+
+exports.truncateRenderModels = functions.database.ref('/bus/render/{renderType}/models/{modelId}').onWrite(event => {
+      const parentRef = event.data.ref.parent;
+return parentRef.once('value').then(snapshot => {
+      if (snapshot.numChildren() >= MAX_LOG_COUNT) {
+  let childCount = 0;
+  const updates = {};
+  snapshot.forEach(function(child) {
+    if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
+      updates[child.key] = null;
+    }
+  });
+  // Update the parent. This effectively removes the extra children.
+  return parentRef.update(updates);
+}
+});
+});
